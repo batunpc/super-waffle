2266757a0f19a716120519c1a700a75e9e1af296
batunpc <batuhnpc@gmail.com>
2022-05-12 23:34:25 -0400

    adding glfw windowing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
+#include "GLFW/glfw3.h"
 #include "adder.h"
 
 int main() {
     std::cout << "Hello World\n";
     std::cout << add(72.1f, 73.8f) << std::endl;
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Draw gears
+        // draw();
+
+        // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+linux command line ez mode
+
+      ./configure.sh
+      ./build.sh
+      ./run.sh
